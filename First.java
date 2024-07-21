faea90810278a820e5f1cf109550dc6463602b11
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-07-21 15:13:27 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,11 @@
+package DAY1;
+
+/**
+ * First
+ */
+public class First {
+
+    public static void main(String[] args) {
+        System.out.println("Hey Viewer its my day 1 of learning java from scratch");
+    }
+}
