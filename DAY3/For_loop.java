48a542575b579fa8910bcc01c067c93fd6c67abb
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-07-23 21:39:19 +0530

    Create For_loop.java

---
+++ DAY3/For_loop.java
@@ -0,0 +1,10 @@
+// Basic implementation of for loop shows that how for loop works @vinaypandey0001
+
+public class For_loop {
+    public static void main(String[] args) {
+        int i;
+        for (i=0;i<3;i++){
+        System.out.println("hello world.");
+        }
+    }
+}
