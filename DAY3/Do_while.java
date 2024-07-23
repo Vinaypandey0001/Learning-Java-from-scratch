ddbc9864a0a84ef1113f931fe84400104bb3c82b
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-07-23 22:25:46 +0530

    Create Do_while.java

---
+++ DAY3/Do_while.java
@@ -0,0 +1,13 @@
+// Program to show how do-while loop works 
+
+public class Do_while {
+    public static void main(String[] args) {
+
+    int i=0; //here we initialize the value of i
+    do{
+        System.out.println(i);
+        i++; // here we increment  the value of i by 1
+    }
+    while(i<5); // At last condition will checks  after the initialization and updation.
+}
+}
