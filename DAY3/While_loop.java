c518a518d19785f289a71ef4e5ee9683fc4e7ca5
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-07-23 22:14:05 +0530

    Create While_loop.java
    A simple program to show how while loop works

---
+++ DAY3/While_loop.java
@@ -0,0 +1,13 @@
+// Implementation of while loop
+
+public class While_loop {
+    public static void main(String[] args) {
+        int i=0; // here we initialize the value to varriable
+
+        while (i<5) // here we write the condition
+        {
+            System.out.println("hello vinay");
+            i++; // here we will increment it 
+        }
+    }
+}
