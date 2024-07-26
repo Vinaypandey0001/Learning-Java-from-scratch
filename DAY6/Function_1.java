d2922740521fa595326105ee30fcfa2024ecb6f8
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-07-26 18:58:24 +0530

    Create Function_1.java

---
+++ DAY6/Function_1.java
@@ -0,0 +1,11 @@
+//Simple program to create a function  @vinaypandey0001
+
+public class Function_1 {
+    public static void MyName(){ //MyName is a function declared over here
+        System.out.println("hello my first function called");
+    }
+    public static void main(String[] args) {
+        MyName(); // here that function calls inside the main function 
+    }
+}
+ // output = hello my first function called
