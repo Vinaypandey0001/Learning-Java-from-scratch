21a59c31d020d989cfdbe7a55e5522e82f45b08e
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-07-28 21:52:04 +0530

    Create Array_2.java

---
+++ DAY8/Array_2.java
@@ -0,0 +1,20 @@
+// Java Program to demonstrate Java Multidimensional Array @vinaypandey0001
+
+import java.io.*;
+
+class Array_2 {
+    public static void main(String[] args)
+    {
+        // Syntax
+        int[][] arr = new int[3][3];
+        // 3 row and 3 column
+
+          // Number of Rows
+        System.out.println("Number of Rows:"+
+                           arr.length);
+      
+          // Number of Columns
+        System.out.println("Number of Columns:"+
+                           arr[0].length);
+    }
+}
