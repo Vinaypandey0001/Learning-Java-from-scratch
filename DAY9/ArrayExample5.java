977c8892868064c5f3a55edf4ffabe78acf25213
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-07-29 22:31:00 +0530

    Create ArrayExample5.java
    fix the issues contribute over here

---
+++ DAY9/ArrayExample5.java
@@ -0,0 +1,21 @@
+// program to find the specific element in the array  @vinaypandey0001
+
+import java.util.Scanner;
+public class ArrayExample5 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = {1, 2, 2, 3, 4, 2, 5};
+        int target = 0;
+        target =sc.nextInt();
+        int count = 0;
+        
+        for (int number : numbers) {
+            if (number == target) {
+                count++;
+            }
+        }
+        
+        System.out.println("Number " + target + " occurs " + count + " times.");
+    }
+}
