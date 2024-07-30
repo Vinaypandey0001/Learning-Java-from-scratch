b44a53db4b6645915e6686e126886bfea607245e
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-07-30 21:19:03 +0530

    Create Factorial.java

---
+++ DAY10/Factorial.java
@@ -0,0 +1,15 @@
+// Program to print factorial of  a number @vinaypandey0001 
+public class Factorial {
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int number = 5;
+        System.out.println("Factorial of " + number + " is " + factorial(number));
+    }
+}
