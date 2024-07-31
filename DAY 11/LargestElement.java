da3be7b1056e51372fed0ae608ae7bc36508dff0
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-07-31 22:07:45 +0530

    Create LargestElement.java

---
+++ DAY 11/LargestElement.java
@@ -0,0 +1,27 @@
+// This program finds the largest element in an array @vinaypandey0001
+
+import java.util.Scanner;
+
+public class LargestElement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+
+        System.out.println("Largest element in the array: " + max);
+    }
+}
