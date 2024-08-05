a102bdfdc44dfc81284efb49d2a04432d547f6c4
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-08-05 21:50:42 +0530

    Create BubbleSortExample3.java

---
+++ DAY_16/BubbleSortExample3.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class BubbleSortExample3 {
+    public static void bubbleSort(int[] array) {
+        int n = array.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) break;
+
+            System.out.println("After pass " + (i + 1) + ":");
+            for (int num : array) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array:");
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        System.out.println("Before sorting:");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+
+        bubbleSort(array);
+
+        System.out.println("After sorting:");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+    }
+}
