518115ccc320a4b58c59d414bb211f21b3317545
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-08-06 22:16:30 +0530

    Create BasicArray.java

---
+++ DAY_17/BasicArray.java
@@ -0,0 +1,10 @@
+public class BasicArray {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5}; // Array initialization
+
+        // Accessing array elements
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }
+    }
+}
