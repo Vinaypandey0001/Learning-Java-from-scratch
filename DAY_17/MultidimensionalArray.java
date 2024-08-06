ce5def41099c79f8ae9583b86acf94e5549fee41
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-08-06 22:17:34 +0530

    Create MultidimensionalArray.java

---
+++ DAY_17/MultidimensionalArray.java
@@ -0,0 +1,17 @@
+public class MultidimensionalArray {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        // Accessing elements in a 2D array
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
