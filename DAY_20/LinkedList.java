6d9417ea90b17bc8511c45d0152066b96235eaa5
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-08-09 21:54:49 +0530

    Create LinkedList.java

---
+++ DAY_20/LinkedList.java
@@ -0,0 +1,9 @@
+class LinkedList {
+    Node head;
+
+    public void insertAtBeginning(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+}
