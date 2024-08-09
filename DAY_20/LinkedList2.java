a800b99c20ac5e94c2eb8e8695f77caea8c32400
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-08-09 21:56:54 +0530

    Create LinkedList2.java

---
+++ DAY_20/LinkedList2.java
@@ -0,0 +1,18 @@
+class LinkedList2 {
+    Node head;
+
+    public void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+}
