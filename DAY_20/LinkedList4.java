d94c7293f635f7e899eab4d33bfe0df9b9c76eb4
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-08-09 21:58:09 +0530

    Create LinkedList4.java

---
+++ DAY_20/LinkedList4.java
@@ -0,0 +1,14 @@
+class LinkedList4 {
+    Node head;
+
+    public boolean search(int key) {
+        Node temp = head;
+        while (temp != null) {
+            if (temp.data == key) {
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+}
