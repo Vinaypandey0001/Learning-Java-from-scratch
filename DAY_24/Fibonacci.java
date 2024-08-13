2bdf0832abee925966b8e42088d02e197d549983
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-08-13 22:27:03 +0530

    Create Fibonacci.java

---
+++ DAY_24/Fibonacci.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of terms: ");
+        int n = scanner.nextInt();
+
+        int a = 0, b = 1;
+        System.out.print("Fibonacci Sequence: " + a + " " + b);
+
+        for (int i = 2; i < n; i++) {
+            int next = a + b;
+            System.out.print(" " + next);
+            a = b;
+            b = next;
+        }
+    }
+}
