48f9db86c9e2150bf96eaf7ebd7f8c05d77dcfb1
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-08-14 22:58:06 +0530

    Create GCD.java

---
+++ DAY_25/GCD.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class GCD {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter the second number: ");
+        int b = scanner.nextInt();
+
+        int gcd = findGCD(a, b);
+        System.out.println("GCD of " + a + " and " + b + " is " + gcd);
+    }
+
+    public static int findGCD(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return findGCD(b, a % b);
+    }
+}
