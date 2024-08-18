63f8a2099390b202a68a454b49eb5b772e55f91d
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-08-18 21:29:04 +0530

    Create RemoveDuplicatesSortedArray.java

---
+++ DAY_29/RemoveDuplicatesSortedArray.java
@@ -0,0 +1,24 @@
+public class RemoveDuplicatesSortedArray {
+    public static int removeDuplicates(int[] arr) {
+        if (arr.length == 0) return 0;
+        
+        int uniqueIndex = 1;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                arr[uniqueIndex] = arr[i];
+                uniqueIndex++;
+            }
+        }
+        return uniqueIndex;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 2, 3, 4, 4, 5};
+        int newLength = removeDuplicates(arr);
+
+        System.out.println("Array after removing duplicates:");
+        for (int i = 0; i < newLength; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
