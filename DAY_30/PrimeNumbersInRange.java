2b6b95da551b3a15afd047bb3ac0ba895647f9e1
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-08-19 21:54:30 +0530

    Create PrimeNumbersInRange.java

---
+++ DAY_30/PrimeNumbersInRange.java
@@ -0,0 +1,20 @@
+public class PrimeNumbersInRange {
+    public static boolean isPrime(int number) {
+        if (number <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int start = 10, end = 50;
+
+        System.out.println("Prime numbers between " + start + " and " + end + ":");
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
