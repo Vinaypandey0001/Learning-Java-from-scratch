10ded9d87c9890d9605d3d335c37ec7ea08fc20e
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-08-19 21:56:02 +0530

    Create EvenOddCheck.java

---
+++ DAY_30/EvenOddCheck.java
@@ -0,0 +1,11 @@
+public class EvenOddCheck {
+    public static void main(String[] args) {
+        int number = 20;
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+    }
+}
