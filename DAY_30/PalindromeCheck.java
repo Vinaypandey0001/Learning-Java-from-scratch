fa30a51f73d41f60739b97487a0dd8df1a070064
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-08-19 21:52:21 +0530

    Create PalindromeCheck.java

---
+++ DAY_30/PalindromeCheck.java
@@ -0,0 +1,23 @@
+public class PalindromeCheck {
+    public static boolean isPalindrome(int number) {
+        int original = number;
+        int reverse = 0;
+
+        while (number > 0) {
+            int digit = number % 10;
+            reverse = reverse * 10 + digit;
+            number /= 10;
+        }
+
+        return original == reverse;
+    }
+
+    public static void main(String[] args) {
+        int number = 121;
+        if (isPalindrome(number)) {
+            System.out.println(number + " is a palindrome.");
+        } else {
+            System.out.println(number + " is not a palindrome.");
+        }
+    }
+}
