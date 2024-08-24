b1a66ab572cf303b9e174fe775695fb5f1e1a78a
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-08-24 09:09:47 +0530

    Create FindNonRepeatingElements.java

---
+++ DAY_34/FindNonRepeatingElements.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindNonRepeatingElements {
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 4, 5, 8, 9, 7};
+
+        HashMap<Integer, Integer> elementCount = new HashMap<>();
+
+        for (int num : arr) {
+            elementCount.put(num, elementCount.getOrDefault(num, 0) + 1);
+        }
+
+        System.out.println("Non-repeating elements in the array:");
+        for (Map.Entry<Integer, Integer> entry : elementCount.entrySet()) {
+            if (entry.getValue() == 1) {
+                System.out.println(entry.getKey());
+            }
+        }
+    }
+}
