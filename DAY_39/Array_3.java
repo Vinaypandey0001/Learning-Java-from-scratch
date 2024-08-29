7961f1785ff6448ff40a04e153252411a36dd981
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-08-29 13:13:49 +0530

    Create Array_3.java

---
+++ DAY_39/Array_3.java
@@ -0,0 +1,10 @@
+// code for traversal in array in reverse order  @vinaypandey0001
+public class Array_3 {
+    public static void main(String[]args){
+        int marks[]={100,95,90,85,80};
+        for (int i=4; i<marks.length; i--){
+            System.out.println(marks[i]);
+        }
+    }
+}
+
