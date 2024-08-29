9e3a5233bec1fb86043fb1ec1d9bee5cfb442011
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-08-29 13:00:41 +0530

    Create Array_2.java

---
+++ DAY_39/Array_2.java
@@ -0,0 +1,11 @@
+// code for traversal in array @vinaypandey0001
+public class Array_2 {
+    public static void main(String[]args){
+        int marks[]={90,100,80,85,95};
+        for (int i=0; i<marks.length; i++){
+            System.out.println("elements in array are "+ marks[i]);
+        }
+    }
+
+
+}
