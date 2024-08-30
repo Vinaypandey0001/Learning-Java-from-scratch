904b86ff0f09ae5d683683904bd533b649a83b9e
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-08-30 21:42:25 +0530

    Create SumOfElements.java

---
+++ DAY_40/SumOfElements.java
@@ -0,0 +1,21 @@
+public class SumOfElements {
+    public static int findSum(int[][] matrix) {
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                sum += matrix[i][j];
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        int totalSum = findSum(matrix);
+        System.out.println("Sum of all elements: " + totalSum);  // Output: 45
+    }
+}
