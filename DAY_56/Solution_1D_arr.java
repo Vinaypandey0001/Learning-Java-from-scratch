dcc71367847ad3e030bbd85cf1857e7fc100b316
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-09-15 23:25:43 +0530

    Create Solution_1D_arr.java

---
+++ DAY_56/Solution_1D_arr.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Solution_1D_arr {
+
+    public static void main(String[] args) {
+        // Create a scanner object to read input
+        Scanner sc = new Scanner(System.in);
+        
+        // Read the integer n (size of the array)
+        int n = sc.nextInt();
+        
+        // Create an array of size n
+        int[] arr = new int[n];
+        
+        // Read n integers and store them in the array
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        // Temporary print statement for debugging (remove this if the locked code works)
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+        
+        // Close the scanner
+        sc.close();
+    }
+}
