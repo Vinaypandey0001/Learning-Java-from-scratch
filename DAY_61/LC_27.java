968cf194f7d1607b19bd71913b5fda9a20f2e3a5
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-09-20 22:45:01 +0530

    Create LC_27.java

---
+++ DAY_61/LC_27.java
@@ -0,0 +1,15 @@
+class LC_27 {
+    public int removeElement(int[] nums, int val) {
+        int k = 0; 
+        
+        for (int i = 0; i < nums.length; i++) {
+            
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        
+        return k;
+    }
+}
