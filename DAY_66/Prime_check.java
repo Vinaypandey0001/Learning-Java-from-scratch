b02387e66841ef77d1802882c8d6207844389912
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-09-25 23:26:06 +0530

    Create Prime_check.java

---
+++ DAY_66/Prime_check.java
@@ -0,0 +1,44 @@
+public class Prime_check {
+    // Function to check if a
+    // given number is prime.
+    static boolean checkPrime(int n) {
+        // Initialize a counter variable to
+        // count the number of factors.
+        int cnt = 0;
+        // Loop through numbers from 1 to n.
+        for (int i = 1; i <= n; i++) {
+            // If n is divisible by i
+            // without any remainder.
+            if (n % i == 0) {
+                // Increment the counter.
+                cnt = cnt + 1;
+            }
+        }
+
+        // If the number of
+        // factors is exactly 2
+        if (cnt == 2) {
+            // Return true, indicating
+            // that the number is prime.
+            return true;
+        }
+        // If the number of
+        // factors is not 2.
+        else {
+            // Return false, indicating
+            // that the number is not prime.
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 1483;
+        boolean isPrime = checkPrime(n);
+        if (isPrime) {
+            System.out.println(n + " is a prime number.");
+        } else {
+            System.out.println(n + " is not a prime number.");
+        }
+    }
+}
+                  
