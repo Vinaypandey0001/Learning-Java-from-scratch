b9817948321dc0554f723669f781b91b69e43297
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-09-29 22:12:39 +0530

    Create VINNS.java

---
+++ DAY_69/VINNS.java
@@ -0,0 +1,28 @@
+// Java Program to demonstrate Interface
+import java.io.*;
+interface Shape {
+    double getArea();
+    double getPerimeter();
+}
+class Circle implements Shape {
+    private double radius;
+    public Circle(double radius) { this.radius = radius; }
+    public double getArea()
+    {
+        return Math.PI * radius * radius;
+    }
+    public double getPerimeter()
+    {
+        return 2 * Math.PI * radius;
+    }
+}
+class VINNS {
+    public static void main(String[] args)
+    {
+        Circle circle = new Circle(5.0);
+        System.out.println("Area of circle is "
+                           + circle.getArea());
+        System.out.println("Perimeter of circle is"
+                           + circle.getPerimeter());
+    }
+}
