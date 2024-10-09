9d5eeaf8ed43eeb89078a84181034f18cdce2e3a
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-10-09 22:26:48 +0530

    Create Calculate_freq.java

---
+++ DAY_72/Calculate_freq.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Calculate_freq
+{
+public static void Printfrequency(String str)
+  {
+    int[] freq = new int[26];
+    for (int i = 0; i < str.length(); i++)
+    {
+      freq[str.charAt(i) - 'a']++;
+    }
+    for (int i = 0; i < 26; i++)
+    {
+      if (freq[i] != 0)
+      {
+        System.out.print((char)(i + 'a'));
+        System.out.print(freq[i] + " ");
+      }
+    }
+  }
+
+public static void main(String args[])
+  {
+    String str = "takeuforward";
+    Printfrequency(str);
+  }
+}
