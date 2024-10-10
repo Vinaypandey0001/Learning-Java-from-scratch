17cedec1fb17671abcbe6f60529036a27565deef
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-10-10 23:42:56 +0530

    Create Main1.java

---
+++ DAY_73/Main1.java
@@ -0,0 +1,13 @@
+public class Main1 {
+  public static void main(String args[]) {
+    int num1 = 1;
+    int num2 = 3;
+    
+    // Using if-else to find the greatest number
+    if (num1 > num2) {
+      System.out.println("The greatest of the two numbers is " + num1);
+    } else {
+      System.out.println("The greatest of the two numbers is " + num2);
+    }
+  }
+}
