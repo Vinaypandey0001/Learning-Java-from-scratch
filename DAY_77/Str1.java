fb88bfadd304a85f73110b031b8540c11dc74ca6
Vinay Kumar  Pandey <134824731+Vinaypandey0001@users.noreply.github.com>
2024-10-28 21:14:09 +0530

    Create Str1.java

---
+++ DAY_77/Str1.java
@@ -0,0 +1,19 @@
+
+import java.io.*;
+
+class Str1 {
+
+    public static void main(String[] args)
+    {
+     
+        StringBuffer s = new StringBuffer("Vinay");
+
+     
+        s.append("Pandey");
+
+        System.out.println(s);
+
+        s.append(1);
+        System.out.println(s);
+    }
+}
